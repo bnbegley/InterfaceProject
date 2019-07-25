9907df12a724429517aed53f64dee8fbe2534e52
brail33na11@gmail.com <brail33na11@gmail.com>
2019-07-25 14:16:26 -0400

    Add project files.

---
+++ InterfaceProject/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace InterfaceProject {
+    class Program {
+        static void Main(string[] args) {
+
+            IStudent greg = new FakeGreg();
+            Student[] students = greg.GetStudents();
+            int idx = 0;
+                while (idx < students.Length) {
+                Console.WriteLine($"Student name is {students[idx].Name}.");
+                idx = idx + 1; 
+            }
+
+        }
+    }
+
+    class Student {
+        public string Name { get; set; } 
+    }
+
+   interface IStudent {
+        Student[] GetStudents();
+    }
+    class FakeGreg: IStudent {
+
+        public Student[] GetStudents () {
+            Student[] students = new Student[] {
+                new Student {Name = "Denise"},
+                new Student {Name = "Kim"},
+                new Student {Name = "Greg"}
+
+            }; 
+            return students;
+        }
+    }
+}
